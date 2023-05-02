79169223eaeb7aca4c1ee9a2459895380f0f2578
Moses <lazaromoses77@gmail.com>
2023-05-02 16:55:27 +0300

    Adding Routes

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/products', function () {
+    return ('Products');
+});
 
